22bd60c928c7c90ad543b318abb7de3ea5bddc78
marygwcx <marijaagejevaite@gmail.com>
2025-01-05 16:20:48 +0200

    .

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -21,4 +21,12 @@ public class Player : MonoBehaviour
         transform.position += new Vector3(x, 0, z).normalized * speed * Time.deltaTime;
 
     }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            print("Game over");
+            transform.position = startPos;
+        }
+    }
 }
