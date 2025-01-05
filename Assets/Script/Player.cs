f85968de429a430d3dc6dd4d5eb18d0aeb3603e7
marygwcx <marijaagejevaite@gmail.com>
2025-01-05 16:13:59 +0200

    Player and movement script, wall prefab

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Vector3 startPos;
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        
+        var x = Input.GetAxisRaw("Horizontal");
+        var z = Input.GetAxisRaw("Vertical");
+
+       
+        transform.position += new Vector3(x, 0, z).normalized * speed * Time.deltaTime;
+
+    }
+}
