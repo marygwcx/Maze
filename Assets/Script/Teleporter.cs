084885d1a52127785eb0e4d784e54064888b5bfd
marygwcx <marijaagejevaite@gmail.com>
2025-01-12 16:15:08 +0200

    Fake teleporter, level 2

--- Assets/Script/Teleporter.cs
+++ Assets/Script/Teleporter.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleporter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneName;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
